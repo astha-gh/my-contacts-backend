f1a2f15557eed1affd0569416a4411f7529198b5
astha-gh <asthadevadiga7@gmail.com>
2025-06-11 20:29:43 +0530

    Changes in server.js

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require("cors");
 const errorHandler = require('./middleware/errorHandler');
 const Connectdb = require('./config/dbConnection');
-const dotenv = require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 8080;
-const cors = require("cors");
-
 
 Connectdb();
 
+
 app.use(cors());
-app.use(express.json())
-app.use('/api/contact', require("./routes/contactRoutes"));
-app.use('/api/users', require("./routes/userRoutes"));
-app.use(errorHandler);
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Server is running ✅");
 });
-  
+
+app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+app.use('/api/contact', require("./routes/contactRoutes"));
+app.use('/api/users', require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server is listening on port ${port}`);
 });
