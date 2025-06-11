203e079b5eebaa0e43bff4b9d4445a094c85d769
astha-gh <asthadevadiga7@gmail.com>
2025-06-11 19:51:01 +0530

    Installed bycrptjs

--- mycontacts-backend/controller/userController.js
+++ mycontacts-backend/controller/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 
 //@description register a contacts
 //@routes POST /api/users/register
