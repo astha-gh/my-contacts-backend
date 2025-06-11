138418a53a3a9d5da9d65788b001be85070147eb
astha-gh <asthadevadiga7@gmail.com>
2025-06-11 19:34:28 +0530

    FIxed typo

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const errorHandler = require('./middleware/errorhandler');
+const errorHandler = require('./middleware/errorHandler');
 const Connectdb = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const cors = require("cors");
 
 
